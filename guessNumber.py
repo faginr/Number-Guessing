1de776290e2057078695e6a3e886fd72f7bea445
Rex Fagin <rexwfagin@gmail.com>
2019-05-26 13:49:49 -0700

    Fixed incorrect indentation which caused a premature loop exit in the code.

--- guessNumber.py
+++ guessNumber.py
@@ -6,7 +6,8 @@ print("I am thinking of a number between 1 and 100.")
 
 #Ask for a guess
 
-for guessTally in range(1,7):
+x = range(1,7,1)
+for guessTally in x:
     print("Make a guess:")
     guess = int(input())
 
@@ -16,9 +17,9 @@ for guessTally in range(1,7):
         print("Too High!")
     else:
         break
-
-    if guess == secretNumber:
-        print("Correct! You guessed correctly in " + str(guessTally) + " guesses.")
-    else:
-        print("Wrong! The number was " + str(secretNumber))
+#Indentations matter, make sure to indent blocks properly or things will not work!
+if guess == secretNumber:
+    print("Correct! You guessed correctly in " + str(guessTally) + " guesses.")
+else:
+    print("Wrong! The number was " + str(secretNumber))
         
